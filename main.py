48f197b06939f92d57f1b3de10cabd78631ae856
kuuuj <kuuujin@gmail.com>
2023-05-26 21:00:10 -0500

    templates

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
 @app.route('/')
 def hello_world():
     return render_template('index.html', message='Hello, World!')
@@ -12,9 +15,16 @@ def render_form():
         text = request.form.get('text')
         if request.form['submit_button'] == 'Lowercase':
             message = text.lower()
-        elif request.form['submit_button'] == 'Capital':
+        elif request.form['submit_button'] == 'Uppercase':
             message = text.upper()
+        elif request.form['submit_button'] == 'Count':
+            count = len(text.split())
+            message = f"Total number of words: {count}"
     return render_template('form.html', message=message)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
